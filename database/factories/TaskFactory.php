dfeeb06538c78595eca22efd61b2018bc27b4463
Jesse <9021012@student.zadkine.nl>
2024-03-25 13:20:05 +0100

    feature-tasks-admin-factory
    feature-tasks-admin-factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -6,6 +6,9 @@ use App\Models\Activity;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\seeder;
 use App\Models\Project;
+use App\Models\Task;
+use App\Models\User;
+use Carbon\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
@@ -21,7 +24,9 @@ class TaskFactory extends Factory
     {
         return [
             "task" => $this->faker->word(),
-            "begindate" => $this->faker->date(),
+            "begindate" => Carbon::today()->subDays(random_int(0, 365)),
+            "enddate" => Carbon::today()->subDays(random_int(0, 365)),
+            "user_id" => User::all()->random()->id,
             "project_id" => Project::all()->random()->id,
             "activity_id" => Activity::all()->random()->id,
         ];
